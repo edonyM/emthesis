1fe6c8b97581ef1e1886188951687d0598ae4bd6
edonyM <edonyzpc@gmail.com>
2015-12-13 23:15:54 +0800

    add thesis plot code

--- code/k_h_means.py
+++ code/k_h_means.py
@@ -17,9 +17,9 @@ r"""
  #
  # twitter : @edonyzpc
  #
- # Last modified: 2015-12-10 18:42
+ # Last modified: 2015-12-13 18:36
  #
- # Filename: pca3.py
+ # Filename: k_h_means.py
  #
  # Description: All Rights Are Reserved
  #
@@ -30,9 +30,8 @@ r"""
 import matplotlib.pyplot as plt
 #from mpl_toolkits.mplot3d import Axes3D as Ax3
 #from scipy import stats as st
-from matplotlib import cm
+#from matplotlib import cm
 import numpy as np
-import csv
 
 class PyColor(object):
     """ This class is for colored print in the python interpreter!
@@ -87,46 +86,41 @@ class PyColor(object):
         self.warningcolor = ''
         self.endcolor = ''
 
+filebuf = open('k_means_data.txt')
+filebuf2 = open('centroids.txt')
+x = []
+y = []
+c_x1 = []
+c_y1 = []
+c_x2 = []
+c_y2 = []
+c_x3 = []
+c_y3 = []
+for line in filebuf.readlines():
+    line = line.rstrip().split()
+    x.append(float(line[0]))
+    y.append(float(line[1]))
+for line in filebuf2.readlines():
+    line = line.rstrip().split()
+    c_x1.append(float(line[0]))
+    c_x2.append(float(line[2]))
+    c_x3.append(float(line[4]))
+    c_y1.append(float(line[1]))
+    c_y2.append(float(line[3]))
+    c_y3.append(float(line[5]))
 
-fig = plt.figure('Projection of Principle Component')
+fig = plt.figure('K-Harmonic Means')
+ax = fig.add_subplot(111)
 ax1 = fig.add_subplot(111)
 ax2 = fig.add_subplot(111)
 ax3 = fig.add_subplot(111)
-ax4 = fig.add_subplot(111)
-ax5 = fig.add_subplot(111)
-ax6 = fig.add_subplot(111)
-ax7 = fig.add_subplot(111)
-ax8 = fig.add_subplot(111)
 
-filebuf = open('plane.csv')
-data_csv = csv.reader(filebuf)
-
-para = []
-for row in data_csv:
-    [para.append(float(i)) for i in row]
-
-mu, sigma = 0.5, 0.5
-x_point = np.linspace(-100, 100, 20)
-y_point = np.linspace(-100, 100, 40)
-x = [i + 1.5*np.random.normal(0.3, 4.5) for i in x_point]
-y = [i + 3*np.random.normal(0, 0.5) for i in y_point]
-
-x_point, y_point = np.meshgrid(x_point, y_point)
-z_point = (-x_point*para[0] - y_point*(0.01 + para[1]) - para[3])/para[2] + np.random.normal(mu,sigma, (40, 20))/8
-
-x = [i + 1.5*np.random.normal(0.3, 4.5) for i in x_point]
-y = [i + 3*np.random.normal(0, 0.5) for i in y_point]
-print(len(x[0]))
-
-ax1.scatter(x[:][:5], y[:][:5], color='cyan', marker='.', label='1th Nearest Neighbor')
-ax2.scatter(x[:][5:10], y[5:10], color='green', marker='.', label='2th Nearest Neighbor')
-ax3.scatter(x[:][10:15], y[:][10:15], color='yellow', marker='.', label='3th Nearest Neighbor')
-ax4.scatter(x[:][15:20], y[:][15:20], color='blue', marker='.', label='4th Nearest Neighbor')
-ax5.scatter(x[:][20:25], y[:][20:25], color='pink', marker='.', label='5th Nearest Neighbor')
-ax6.scatter(x[:][25:30], y[:][25:30], color='orange', marker='.', label='6th Nearest Neighbor')
-ax7.scatter(x[:][30:35], y[:][30:35], color='black', marker='.', label='7th Nearest Neighbor')
-ax8.scatter(x[:][35:], y[:][35:], color='brown', marker='.', label='8th Nearest Neighbor')
-ax1.set_xlabel('u')
-ax1.set_ylabel('v')
-plt.legend(fontsize='small')
+ax.scatter(x, y, color='green', marker='s', linewidth=2)
+ax1.plot(c_x1, c_y1, color='black', marker='^', linewidth=2, label='1th Centroid', ms=10)
+#ax1.annotate('Convergence Direction',xy=(c_x1[0], c_y1[0]), xytext=(3.5,2.5), arrowprops=dict(facecolor='black', shrink=0.15))
+ax2.plot(c_x2, c_y2, color='blue', marker='d', linewidth=2, label='2th Centroid', ms=10)
+ax3.plot(c_x3, c_y3, color='brown', marker='o', linewidth=2, label='3th Centroid', ms=10)
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+plt.legend(fontsize='medium')
 plt.show()
